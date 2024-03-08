7ce60d18fbca3a13887c82ce311de1066f508976
Doctorstrange <doctorstrange247@gmail.com>
2024-03-08 03:13:28 +0000

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,10 +8,10 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    pascal = [[1]]
     for i in range(1, n):
-        row = [1] + [triangle[i - 1][j - 1] +
-                     triangle[i - 1][j] for j in range(1, i)] + [1]
-        triangle.append(row)
+        row = [1] + [pascal[i - 1][j - 1] +
+                     pascal[i - 1][j] for j in range(1, i)] + [1]
+        pascal.append(row)
 
-    return triangle
+    return pascal

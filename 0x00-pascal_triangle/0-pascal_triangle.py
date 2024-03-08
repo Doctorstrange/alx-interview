d5a360bba3a36bb9997d3dcec01e0c06efe09c42
Doctorstrange <doctorstrange247@gmail.com>
2024-03-08 03:05:47 +0000

    10-car.js

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,16 +5,18 @@
 def pascal_triangle(n):
     """list of lists of integers representing the Pascal’s triangle"""
 
-    if n <= 0:
-        return []
+  if n <= 0:
+    return []
 
-    triangle = [[1]]
+  triangle = [[1]]  # Initialize with first row (1)
 
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
-        row.append(1)
-        pascal.append(row)
+  for i in range(1, n):
+    # Build the next row based on the previous row
+    row = [1]  # Start with 1 at the beginning
+    for j in range(1, i):
+      # Get the sum of elements above us in the previous row
+      row.append(triangle[i-1][j-1] + triangle[i-1][j])
+    row.append(1)  # End with 1
+    triangle.append(row)
 
-    return pascal
+  return triangle

39f2bc3ebc217fc643d2bfe7eb3d638c7f67c273
Doctorstrange <doctorstrange247@gmail.com>
2024-03-08 03:11:15 +0000

    py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,7 +10,8 @@ def pascal_triangle(n):
 
     triangle = [[1]]
     for i in range(1, n):
-        row = [1] + [triangle[i - 1][j - 1] + triangle[i - 1][j] for j in range(1, i)] + [1]
+        row = [1] + [triangle[i - 1][j - 1] +
+                     triangle[i - 1][j] for j in range(1, i)] + [1]
         triangle.append(row)
 
     return triangle

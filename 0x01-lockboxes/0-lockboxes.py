bdf6145faefebf483b1f43d978361e98c6650f2e
Doctorstrange <doctorstrange247@gmail.com>
2024-03-15 02:17:19 +0000

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Returns the string representation of the given float `n`."""
+
+
+def canUnlockAll(boxes):
+    """
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes or len(boxes) == 1:
+        return True
+
+    opened_boxes = set([0])
+
+    keys_to_check = boxes[0]
+
+    while keys_to_check:
+        current_key = keys_to_check.pop()
+
+        if current_key < len(boxes) and current_key not in opened_boxes:
+            opened_boxes.add(current_key)
+            keys_to_check.extend(boxes[current_key])
+
+    return len(opened_boxes) == len(boxes)

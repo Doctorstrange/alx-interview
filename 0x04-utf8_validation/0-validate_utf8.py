d4da13ee4f306bc75c36bab1edbd250a7d90fd82
doctorstrange <doctorstrange@gmail.com>
2024-04-08 00:05:35 +0000

    returns leading set bits number"

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""determines if a given data set represents a valid UTF-8 encoding."""
+"""UTF-8 Validation"""
 
 
-def count_leading_ones(set):
-    """Returns number of leading set"""
-    count = 0
-    cover = 1 << 7
-    while set & cover:
-        count += 1
-        cover >>= 1
-    return count
+def get_bits(num):
+    """returns leading set bits number"""
+    set_bits = 0
+    bit = 1 << 7
+    while bit & num:
+        set_bits += 1
+        bit = bit >> 1
+    return set_bits
 
 
 def validUTF8(data):
-    """valid UTF-8 encoding representation"""
-    for set in data:
-        leading_ones = count_leading_ones(set)
-
-        if leading_ones == 0:
-            continue
-
-        if leading_ones == 1 or leading_ones > 4:
-            return False
-
-        for i in range(1, leading_ones):
+    """determines if data set is valid UTF-8 encoding"""
+    count = 0
+    for i in range(len(data)):
+        if count == 0:
+            count = get_bits(data[i])
+            if count == 0:
+                continue
+            if count == 1 or count > 4:
+                return False
+        else:
             if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
                 return False
-
-    return True
+        count -= 1
+    return count == 0

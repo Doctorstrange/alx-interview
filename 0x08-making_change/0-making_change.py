944bfb61cff561e40655e9ac8227874e2931fb10
doctorstrange <doctorstrange@gmail.com>
2024-05-03 02:33:06 +0000

    hold

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,15 +8,15 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    check = 0
+    hold = 0
     temp = 0
     coins.sort(reverse=True)
-    for i in coins:
-        while check < total:
-            check += i
+    for x in coins:
+        while hold < total:
+            hold += x
             temp += 1
-        if check == total:
+        if hold == total:
             return temp
-        check -= i
+        hold -= x
         temp -= 1
     return -1

ebd784a5027c016f318eafe916446947095262a1
doctorstrange <doctorstrange@gmail.com>
2024-05-17 02:22:24 +0000

    0. Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module for Prime Game"""
+
+
+def isWinner(x, nums):
+    """
+    Return: name of the player that won the most rounds
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+    ben = 0
+    maria = 0
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """
+    carry out called function
+    """
+    for x in range(2, len(ls)):
+        try:
+            ls[x * x] = 0
+        except (ValueError, IndexError):
+            break
